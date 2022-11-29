eae5541f5d88f23cc555ff5080483bb3915afc9c
Nathan Malone <nathanamalone@gmail.com>
2022-11-29 14:05:29 -0600

    Exercise 13

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,7 +1,12 @@
 SELECT
     il.InvoiceLineId,
-    t.Name
+    t.Name,
+    ar.Name
 FROM InvoiceLine il
 JOIN Track t
     ON t.TrackId = il.TrackId
+JOIN Album al
+    on al.AlbumId = t.AlbumId
+JOIN Artist ar
+    ON ar.ArtistId = al.ArtistId
 ORDER BY il.InvoiceLineId
