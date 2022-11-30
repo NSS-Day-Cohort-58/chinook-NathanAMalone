adac22afe7e4e0886caab5273f2bae9cdc267466
Nathan Malone <nathanamalone@gmail.com>
2022-11-30 09:54:13 -0600

    Exercise 20

--- top_agent.sql
+++ top_agent.sql
@@ -1,9 +1,4 @@
-SELECT
-    EmployeeFullName,
-    MAX(TotalSum) MaxTotalSum
-FROM
-    (
-    SELECT
+   SELECT
         e.FirstName || ' ' || e.LastName EmployeeFullName,
         SUM(i.Total) TotalSum
     FROM Employee e
@@ -11,10 +6,6 @@ FROM
         ON c.SupportRepId = e.EmployeeId
     JOIN Invoice i
         ON i.CustomerId = c.CustomerId
-    WHERE i.InvoiceDate LIKE "%2009%"
     GROUP BY EmployeeFullName
     ORDER BY TotalSum DESC
     LIMIT 1
-    )
-GROUP BY EmployeeFullName
-ORDER BY MaxTotalSum DESC
